10d30332bd7c78a77f5a815e42846ca574af64ce
haram082 <haramkim082@gmail.com>
2024-03-07 23:50:31 -0800

    data

---
+++ evaluation.r
@@ -0,0 +1,19 @@
+library('pROC')
+
+sr_evaluate <- function(preds_file) {
+    cat(paste('%%%%%%%%%%%%%%%%', preds_file, '%%%%%%%%%%%%%%%%\n'))
+    data <- read.csv(preds_file, sep='\t');
+    cat('==== mean absolute error ====\n')
+    print(t.test(abs(data$p - data$pp), abs(data$p - mean(data$p)), alternative='l'))
+    cat('==== area under the ROC curve ====\n')
+    print(roc(round(p) ~ pp, data=data));
+    print(wilcox.test(round(data$p), data$pp, alternative='g'))
+    cat('==== half-life correlation ====\n')
+    print(cor.test(data$h, data$hh, method='s'))
+}
+
+
+
+sr_evaluate("results/lr/preds")
+sr_evaluate("results/leitner/preds")
+sr_evaluate("results/pimsleur/preds")
